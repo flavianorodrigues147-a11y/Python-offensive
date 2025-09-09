ab196c514b7fb18d2617441deb3acf7abccbba26
Flaviano Rodrigues dos Santos <flavianorodrigues147@gmail.com>
2025-09-08 22:19:25 -0300

    Função e manipulação de strings.

---
+++ atividade02.py
@@ -0,0 +1,17 @@
+# Manipulação de strings
+# Crie uma variável chamada “nome” e atribua a ela o valor do seu nome.
+# Imprima o comprimento do nome utilizando a função “len”.
+# Converta o nome para letras maiúsculas utilizando o método “upper”.
+# Imprima o nome em letras maiúsculas.
+
+# Atribuindo o nome à variável
+nome = "Flaviano"
+
+# Imprimindo o comprimento do nome
+print(f'Comprimento do nome: {len(nome)}')
+
+# Convertendo para letras maiúsculas
+nome_maiusculo = nome.upper()
+
+# Imprimindo o nome em letras maiúsculas
+print(f'Nome em letras maiúsculas: {nome_maiusculo}')
