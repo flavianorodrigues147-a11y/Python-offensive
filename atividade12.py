69b51c8f53662935baf6f6dfc6ae6130eb1ba3af
Flaviano Rodrigues dos Santos <flavianorodrigues147@gmail.com>
2025-09-21 14:52:52 -0300

    atividade para ver o segundo número maior da lista

---
+++ atividade12.py
@@ -0,0 +1,10 @@
+numeros = [32, 10, 58, 30, 55, 12, 28, 51]
+
+numeros.sort()
+# sort() organiza a lista em ordem crescente
+segundo_maior = numeros[-2]
+
+print("O segundo maior valor da lista é:", segundo_maior)
+
+# output:
+# O segundo maior valor da lista é: 55
