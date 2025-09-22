e2879a652dd40d873b2cc5cce0b894b8ab73f339
Flaviano Rodrigues dos Santos <flavianorodrigues147@gmail.com>
2025-09-22 13:46:05 -0300

    atividades simples em python

---
+++ atividade13.py
@@ -0,0 +1,18 @@
+numero = int(input('Digite o seu numero:'))
+
+print(f'O seu número escolhido foi: {numero}')
+
+numero = int(input('Digite o seu numero:'))
+numero2 = int(input('Digite seu segundo numero:'))
+
+soma = numero + numero2
+
+print(f'A soma dos seus dois números é igual a {soma}')
+
+nota1 = float(input('Digite a primeira nota:'))
+nota2 = float(input('Digite a segunda nota:'))
+nota3 = float(input('Digite a terceira nota:'))
+
+media = (nota1 + nota2 + nota3) / 3
+
+print(f'A media do bimestre vai ser igual a: {media}')
